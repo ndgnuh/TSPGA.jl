66de31c8f9302ffc6cbd62b2f277435da5350d08
ndgnuh <ndgnuh@protonmail.com>
2021-07-11 17:19:32 +0700

    Performance tune

--- src/TSPGA.jl
+++ src/TSPGA.jl
@@ -1,100 +1,136 @@
 module TSPGA
 
 using Random
+using Statistics
 using Evolutionary
 using LightGraphs
 using GraphPlot
-include("Example.jl"); using .Examples
+include("Example.jl")
+using .Examples
 include("plot.jl")
+include("other.jl")
 
-struct TSPObject{T} <: Function
-	W::Matrix{T}
-	TSPObject(W::AbstractMatrix{T}) where T = new{T}(W)
-	TSPObject(G::AbstractGraph) = TSPObject(adjacency_matrix(G))
-end
-struct TSPCrossover{T} <: Function
-	W::Matrix{T}
-	TSPCrossover(W::AbstractMatrix{T}) where T = new{T}(W)
-	TSPCrossover(G::AbstractGraph) = TSPCrossover(adjacency_matrix(G))
-end
 
 function p2edge(p)
-	c = Iterators.take(Iterators.cycle(p), length(p) + 1)
-	zip(c, Iterators.drop(c, 1))
+    #= p′ = [p[2:end]; first(p)] =#
+    n = length(p)
+    p′ = Iterators.map(i -> i % n + 1, p)
+    zip(p, p′)
 end
 
-function (f::TSPObject)(p)
-	sum(f.W[I[1], I[2]] for I in p2edge(p))
+
+function firstequal(x, X)
+    for i in eachindex(X)
+        if X[i] == x
+            return i
+        end
+    end
 end
 
-function (f::TSPCrossover)(p1, p2)
-	n = size(f.W, 1)
-	c1 = [[i, j] for (i, j) in p2edge(p1)]
-	c2 = [[i, j] for (i, j) in p2edge(p2)]
-	w1 = [f.W[i, j] for (i, j) in c1]
-	w2 = [f.W[i, j] for (i, j) in c2]
-	perm1 = sortperm(w1)[1:rand(1:n÷4)]
-	perm2 = sortperm(w2)[1:rand(1:n÷4)]
-	cities1 = unique(reduce(vcat, c1[perm1]))
-	cities2 = unique(reduce(vcat, c2[perm2]))
-	child1 = [cities1; setdiff(p2, cities1)]
-	child2 = [cities2; setdiff(p1, cities2)]
-	[child1, child2]
+function TSPObject(W::AbstractMatrix{T}) where {T}
+    function (x)
+        (W[last(x), first(x)] + sum(W[i, j] for (i, j) in zip(x, Iterators.drop(x, 1))))
+    end
 end
+TSPObject(G::AbstractGraph) = TSPObject(adjacency_matrix(G))
+
+function first_few(k, W, p, n)
+    sel = first(sortperm([W[i, j] for (i, j) in p2edge(p)]), k)
+    append!(sel, sel .% n .+ 1)
+    unique!(sel)
+    p[sel]
+end
+
+function full_crossover(W::AbstractMatrix{S}, T = Int) where {S}
+    n::T = size(W, 1)
+    m::T = max(n ÷ 4, one(T))
+    function (p1, p2)
+        k1, k2 = rand(1:m, 2)
+        c1 = first_few(k1, W, p1, n)
+        c2 = first_few(k2, W, p2, n)
+        append!(c1, setdiff(p2, c1))
+        append!(c2, setdiff(p1, c2))
+        [c1, c2]
+    end
+end
+
+function TSPCrossover(W::AbstractMatrix{S}) where {S}
+    n::Int = size(W, 1)
+    cycleind(i) = i % n + 1
+    #= m = max(n÷2, 1) =#
+    m = n - 1
+    function breed(p1::AbstractVector{T}, p2::AbstractVector{T}) where {T<:Integer}
+        w = [W[i, j] for (i, j) in p2edge(p1)]
+        sel = sortperm(p1, by=i -> w[i])[1:rand(1:m)]
+        c = p1[sel]
+        append!(c, setdiff(p2, c))
+        [c, p1[p2]]
+    end
+end
+TSPCrossover(G::AbstractGraph) = TSPCrossover(adjacency_matrix(G))
 
 function mutate(p)
-	p = copy(p)
-	i, j = rand(p, 2)
-	p[i], p[j] = p[j], p[i]
-	return p
+    i, j = rand(eachindex(p), 2)
+    p[i], p[j] = p[j], p[i]
+    return p
 end
 
-function initialize_objective(f, x::AbstractArray{T}, S = typeof(f(x))) where T
-	NonDifferentiable{S,typeof(x)}(f, zero(S), zeros(T, length(x)), [0,])
+function initialize_objective(f, x::AbstractArray{T}, S = typeof(f(x))) where {T}
+    NonDifferentiable{S,typeof(x)}(f, zero(S), zeros(T, length(x)), [0])
 end
 
-function solve_tsp(W::AbstractMatrix{T}; sortresult = false, ga_kwargs = Dict(), options_kwargs = Dict()) where T
-	n = size(W ,1)
+function solve_tsp(
+    W::AbstractMatrix{T};
+    sortresult = false,
+    ga_kwargs = Dict(),
+    options_kwargs = Dict(),
+) where {T}
+    n = size(W, 1)
 
-	options = Evolutionary.Options(;successive_f_tol = 30,
-								   iterations = 2000,
-								    store_trace = true,
-								    options_kwargs...
-								   )
+    options = Evolutionary.Options(;
+        successive_f_tol = 30,
+        store_trace = true,
+        options_kwargs...,
+    )
 
-	alg = GA(;crossover = TSPCrossover(W),
-	   selection = rouletteinv,
-	   mutation = mutate,
-	   mutationRate = 0.5,
-	   crossoverRate = 0.5,
-	   epsilon = 0.2,
-	   populationSize = n,
-	   ga_kwargs...)
+    popsize = get(ga_kwargs, :populationSize, min(n, 100))
+    individual = [randperm(n) for _ = 1:popsize]
 
-	individual = [randperm(n) for _ in 1:alg.populationSize]
+    obj = initialize_objective(TSPObject(W), first(individual))
 
-	obj = initialize_objective(TSPObject(W), first(individual))
-	result = Evolutionary.optimize(obj, Evolutionary.NoConstraints(), individual, alg, options)
-	if result.converged && sortresult
-		let r = result.minimizer
-			_, i = findmin(r)
-			if i == 1
-				r
-			elseif i == lastindex(r)
-				reverse(r)
-			end
-			result.minimizer = [r[i:end]; r[1:i-1]]
-		end
-	end
-	return result
+    alg = GA(;
+        crossover = TSPCrossover(W),
+        selection = rouletteinv,
+        mutation = mutate,
+        mutationRate = 0.5,
+        crossoverRate = 0.5,
+        epsilon = 0.2,
+        populationSize = popsize,
+        ga_kwargs...,
+    )
+
+    result =
+        Evolutionary.optimize(obj, Evolutionary.NoConstraints(), individual, alg, options)
+    if result.converged && sortresult
+        let r = result.minimizer
+            _, i = findmin(r)
+            if i == 1
+                r
+            elseif i == lastindex(r)
+                reverse(r)
+            end
+            result.minimizer = [r[i:end]; r[1:(i - 1)]]
+        end
+    end
+    return result
 end
 solve_tsp(G::AbstractGraph; kwargs...) = solve_tsp(adjacency_matrix(G); kwargs...)
 
 function bsolve_tsp(G, n; kwargs...)
-	results = map(1:n) do
-		solve_Tsp(G; kwargs)
-	end
-	findmin(x -> x.minimum, results)[2]
+    results = map(1:n) do
+        solve_Tsp(G; kwargs)
+    end
+    findmin(x -> x.minimum, results)[2]
 end
 
 export Evolutionary, TSPCrossover, TSPObject, solve_tsp, mutate, wgplot
