fdd8d579e25170fa96e8d804b5b4a0565772b597
ndgnuh <ndgnuh@protonmail.com>
2021-07-11 17:33:20 +0700

    test stuffs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,56 @@
 using TSPGA
 using TSPGA.Examples
+using BenchmarkTools
 using Test
+using Random
 
 @testset "Solve" begin 
-	for G in Examples.all_examples
+	for G in Examples.examples
 		solve_tsp(G)
+		@test true
+	end
+	for tsp in Examples.tsp100_examples
+		solve_tsp(tsp.W)
 		@test true
 	end
 end
 
 @testset "Plot" begin
-	for G in Examples.all_examples
+	for G in Examples.examples
 		wgplot(G)
 		@test true
 	end
 end
+
+@testset "Other solver" begin
+	r1 = TSPGA.nna_tsp(Examples.g5)
+	@test true
+	display(r1)
+	r2 = TSPGA.bruteforce_tsp(Examples.g4)
+	@test true
+	display(r2)
+end
+
+GC.gc()
+let n = 1000,
+	#= g = Examples.generate_random_graph(n, 0.1, 3.0, 0.1) =#
+	g = repeat(Examples.tsp100_examples[1].W, 10, 10)
+	@info size(g)
+	bench = (@benchmark solve_tsp($g) samples=2 evals=2 seconds=Inf gctrial=true)
+	display(bench)
+end
+
+@info "Solve sample"
+let r = solve_tsp(Examples.g4)
+	display(r)
+	display(r.trace)
+	display(r.minimizer)
+end
+let rs = map(Examples.tsp100_examples) do tsp
+		r = solve_tsp(tsp.W, ga_kwargs = (populationSize = 100,))
+		r.minimum, tsp.F
+	end
+	display(rs)
+end
+
+
